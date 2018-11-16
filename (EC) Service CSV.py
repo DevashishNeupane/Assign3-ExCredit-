692e6eded4aba817785d48e2cddebe8a5335ef00
DevashishNeupane <42516511+DevashishNeupane@users.noreply.github.com>
2018-11-16 15:16:37 -0600

    Update (EC) Service CSV.py

--- (EC) Service CSV.py
+++ (EC) Service CSV.py
@@ -28,31 +28,33 @@ def insert_values():
     elem = driver.find_element_by_xpath(
         "//*[@id=\"app-layout\"]/div/div/div/div[2]/div/div/div/div/div[2]/div/div/p[2]/a").click()
     time.sleep(3)
-    elem = driver.find_element_by_xpath("//*[@id=\"app-layout\"]/div/div/div/div[3]/div/a/span").click()
-    time.sleep(3)
+    #elem = driver.find_element_by_xpath("//*[@id=\"app-layout\"]/div/div/div/div[3]/div/a/span").click()
+    #time.sleep(3)
 
     for i in range(0, len(my_list)):
+        elem = driver.find_element_by_xpath("//*[@id=\"app-layout\"]/div/div/div/div[3]/div/a/span").click()
+        time.sleep(3)
         service = my_list[i][0]
         description = my_list[i][1]
         location = my_list[i][2]
         service_charge = my_list[i][3]
 
-    elem = driver.find_element_by_xpath("//*[@id=\"id_cust_name\"]/option[2]").click()
+        elem = driver.find_element_by_xpath("//*[@id=\"id_cust_name\"]/option[2]").click()
 
-    elem = driver.find_element_by_id("id_service_category")
-    elem.send_keys(service)
+        elem = driver.find_element_by_id("id_service_category")
+        elem.send_keys(service)
 
-    elem = driver.find_element_by_id("id_description")
-    elem.send_keys(description)
+        elem = driver.find_element_by_id("id_description")
+        elem.send_keys(description)
 
-    elem = driver.find_element_by_id("id_location")
-    elem.send_keys(location)
+        elem = driver.find_element_by_id("id_location")
+        elem.send_keys(location)
 
-    elem = driver.find_element_by_id("id_service_charge")
-    elem.send_keys(service_charge)
+        elem = driver.find_element_by_id("id_service_charge")
+        elem.send_keys(service_charge)
 
-    elem = driver.find_element_by_xpath("//*[@id=\"app-layout\"]/div/div/div/form/button").click()
-    time.sleep(4)
+        elem = driver.find_element_by_xpath("//*[@id=\"app-layout\"]/div/div/div/form/button").click()
+        time.sleep(4)
 
 
 
